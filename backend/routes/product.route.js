46a8cbb6ff97b88838da3b2a4fb3e6dac6c85c55
Harsh Singla <harshsingla1985@gmail.com>
2025-02-25 01:21:05 +0530

    all product api's completed

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,10 +1,24 @@
 import express from "express"
-import { getAllProducts } from "../controllers/product.controller.js"
+import { 
+    getAllProducts,
+    getFeaturedProducts,
+    getProductsByCategory,
+    getRecommendedProducts,
+    createProduct,
+    toggleFeaturedProduct,
+    deleteProduct,
+} from "../controllers/product.controller.js"
 import { protectRoute, adminRoute } from "../middleware/auth.middleware.js"
 
 const router = express.Router()
 
 router.get("/", protectRoute, adminRoute, getAllProducts)
+router.get("/featured", getFeaturedProducts)
+router.get("/category/:category", getProductsByCategory)
+router.get("/recommendeds", getRecommendedProducts)
+router.post("/", protectRoute, adminRoute, createProduct)
+router.patch("/:id", protectRoute, adminRoute, toggleFeaturedProduct)
+router.post("/:id", protectRoute, adminRoute, deleteProduct)
 
 
 export default router
