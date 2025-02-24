695ff9fc52b2c56d1552d9d0316f31e40f41c885
Harsh Singla <harshsingla1985@gmail.com>
2025-02-24 14:49:35 +0530

    login and refresh token created

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -63,7 +63,28 @@ export const  signup = async (req,res)=>{
 };
 
 export const login = async (req,res)=>{
-    res.send("login is called");
+    try{
+        const {email,password} = req.body
+        const user = await User.findOne({email})
+
+        if(user && (await user.comparePassword(password))){
+            const {accessToken , refreshToken} = generateToken(user._id)
+            await storeRefreshToken(user._id,refreshToken)
+            setCookie(res,accessToken,refreshToken)
+            res.json({
+                _id : user._id,
+                name : user.name,
+                email : user.email,
+                role : user.role,
+            })
+        }else{
+            res.status(401).json({message : "Invalid email or password"})
+        }
+    }
+    catch(error){
+        console.log("Error in login controller",error.message)
+        res.status(500).json({error : error.message})
+    }
 };
 
 export const logout = async (req,res)=>{
@@ -79,6 +100,36 @@ export const logout = async (req,res)=>{
         res.json({message: "Logged out successfully"})
     }
     catch(error){
+        console.log({message: "Error in logout controller", error: error.message})
         res.status(500).json({message: "Server Error" , error : error.message})
+    }
+}
+
+export const refreshToken = async (req,res) => {
+    try{
+        const refreshToken = req.cookies.refreshToken
+
+        if(!refreshToken){
+            return res.status(401).json({message: "No Refresh Token provided"})
+        }
+        
+        const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET)
+        const storedToken = await redis.get(`refresh_token:${decoded.userId}`)
+        if(storedToken !== refreshToken){
+            return res.status(401).json({message: "Invalid Refresh Token"}) 
+        }
+
+        const accessToken = jwt.sign({userId:decoded.userId},process.env.ACCESS_TOKEN_SECRET,{"expiresIn":"15m"})
+        res.cookie("accessToken",accessToken,{ 
+            httpOnly : true, 
+            secure : process.env.NODE_ENV === "production", 
+            sameSite : "strict", 
+            maxAge : 15*60*1000
+        })
+        res.json({message : "Token Refreshed Successfully"})
+    }
+    catch(error){
+        console.log("Error in refreshToken controller", error.message)
+        res.status(500).json({message: "Server Error", error : error.message})
     }
 }
