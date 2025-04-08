b6ef66cfaae7bed1c9cff9495bc236a4902427f3
Harsh Singla <harshsingla1985@gmail.com>
2025-04-08 10:31:07 +0530

    fixed errors

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -18,25 +18,24 @@ export const getCartProducts = async (req, res) => {
 };
 
 export const addToCart = async (req, res) => {
-    try{
-        const {productId} = req.body
-        const user = req.user
+	try {
+		const { productId } = req.body;
+		const user = req.user;
 
-        const existingItem = await user.cartItems.find((item) => item.id === productId)
-        if(existingItem){
-            existingItem.quantity += 1
-        }else{
-            user.cartItems.push(productId)
-        }
+		const existingItem = user.cartItems.find((item) => item.id === productId);
+		if (existingItem) {
+			existingItem.quantity += 1;
+		} else {
+			user.cartItems.push(productId);
+		}
 
-        await user.save();
-        res.json(user.cartItems)
-    }
-    catch(error){
-        console.log("Error in addToCart controller: ", error.message)
-        res.status(500).json({message: "Server Error", message : error.message})
-    }
-}
+		await user.save();
+		res.json(user.cartItems);
+	} catch (error) {
+		console.log("Error in addToCart controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
+};
 
 export const removeAllFromCart = async (req, res) => {
 	try {
@@ -52,7 +51,7 @@ export const removeAllFromCart = async (req, res) => {
 	} catch (error) {
 		res.status(500).json({ message: "Server error", error: error.message });
 	}
-}
+};
 
 export const updateQuantity = async (req, res) => {
 	try {
@@ -78,5 +77,4 @@ export const updateQuantity = async (req, res) => {
 		console.log("Error in updateQuantity controller", error.message);
 		res.status(500).json({ message: "Server error", error: error.message });
 	}
-}
-
+};
